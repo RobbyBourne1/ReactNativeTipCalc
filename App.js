8a3fb463900c25d28c938eb6612b1f3180858905
Robby Bourne <robbybourne1@gmail.com>
2018-06-07 20:45:24 -0400

    Variable Tips added

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import Hello from './Hello'
 import { TextInput } from 'react-native-gesture-handler';
 
@@ -8,12 +8,24 @@ export default class App extends React.Component {
     super();
     this.state = {
       inputValue: "",
+      tip: 0.2,
     }
   }
+
+  updateCustomTip(customTip){
+    if (customTip) {
+      this.setState({
+        tip: parseFloat(customTip)/ 100,
+      })
+    }else{
+      this.setState({tip: 0})
+    }
+  }
+
   render() {
     let tip = 0.00
     if (this.state.inputValue) {
-      tip = parseFloat(this.state.inputValue) * .2
+      tip = parseFloat(this.state.inputValue) * this.state.tip
       tip = (Math.round(tip * 100)/ 100).toFixed(2)
     }
     return (
@@ -26,6 +38,31 @@ export default class App extends React.Component {
       placeholder="0.00"
       onChangeText={(text) => this.setState({inputValue: text})}
       />
+      <View style={styles.Button}>
+        <Button
+        title="10%"
+        onPress={() => this.setState({tip: .1})}
+        />
+        <Button
+        title="15%"
+        onPress={() => this.setState({tip: .15})}
+        />
+        <Button
+        title="20%"
+        onPress={() => this.setState({tip: .2})}
+        />
+        <Button
+        title="25%"
+        onPress={() => this.setState({tip: .25})}
+        />
+        <TextInput
+        value={(this.state.tip * 100).toString()}
+        style={styles.customTip}
+        keyboardType="numeric"
+        placeholder="20%"
+        onChangeText={customTip => this.updateCustomTip(customTip)}
+        />
+        </View>
       </View>
     );
   }
@@ -42,6 +79,17 @@ const styles = StyleSheet.create({
   input:{
     height: 40,
     width: '100%',
+    borderColor: '#333',
+    borderWidth: 1,
+    padding: 5,
+  },
+  Button:{
+    flexDirection: 'row',
+
+  },
+  customTip:{
+    height: 30,
+    width: 60,
     borderColor: '#333',
     borderWidth: 1,
     padding: 5,
